dbb8c1f7dd4f945ff5c955be20deb0ce08c43ae1
Wei Lu <luwei.here@gmail.com>
2012-12-01 17:42:08 +0800

    Refactor

--- removeZoho.js
+++ removeZoho.js
@@ -1,14 +1,24 @@
+removeZoho = function(addedNode){
+  if(addedNode.className != "nH hh")
+    return;
+  if(addedNode.parentNode == null)
+    return;
+  addedNode.parentNode.removeChild(addedNode)
+}
+
+mutationCallback = function(mutation){
+  addedNodes = mutation.addedNodes;
+  if(addedNodes.length > 0){
+    for(var i=0; i<addedNodes.length; i++){
+      removeZoho(addedNodes[i]);
+    }
+  }
+}
+
 window.onload = function(){
   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
   observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(m){
-      if(m.addedNodes.length > 0 && m.addedNodes[0].className == "nH hh"){
-        var zoho = m.addedNodes[0]
-        if(zoho.parentNode == null)
-          return;
-        zoho.parentNode.removeChild(zoho)
-      }
-    })
+    mutations.forEach(mutationCallback);
   });
-  observer.observe(document, { childList: true, subtree: true});
+  observer.observe(document, { childList: true, subtree: true });
 }
