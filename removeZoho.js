f095d9b0130a02c0cc33da11504228edf5378193
Wei Lu <luwei.here@gmail.com>
2012-12-02 10:35:43 +0800

    Look for zoho iframe & textContent instead of class

--- removeZoho.js
+++ removeZoho.js
@@ -1,20 +1,32 @@
-removeZoho = function(addedNode){
-  if(addedNode.className != "nH hh")
-    return;
-  if(addedNode.parentNode == null)
-    return;
-  addedNode.parentNode.removeChild(addedNode)
+function hasZohoIframe(node){
+  if(!(node instanceof Element))
+    return false;
+  var iframes = node.getElementsByTagName('iframe');
+  if(iframes.length == 0)
+    return false;
+
+  var hasIframe = Array.prototype.slice.call(iframes).some(function(iframe){
+    return iframe.src.indexOf('gapps.zoho.com') > -1;
+  });
+
+  return hasIframe && node.textContent.indexOf('Zoho') == 0;
 }
 
-mutationCallback = function(mutation){
-  addedNodes = mutation.addedNodes;
+function hideZoho(addedNode){
+  if(!hasZohoIframe(addedNode))
+    return;
+  addedNode.style.cssText = 'display: none';
+}
+
+function mutationCallback(mutation){
+  var addedNodes = mutation.addedNodes;
   if(addedNodes.length > 0)
-    Array.prototype.slice.call(addedNodes).forEach(removeZoho)
+    Array.prototype.slice.call(addedNodes).forEach(hideZoho);
 }
 
 window.onload = function(){
   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-  observer = new MutationObserver(function(mutations) {
+  var observer = new MutationObserver(function(mutations) {
     mutations.forEach(mutationCallback);
   });
   observer.observe(document, { childList: true, subtree: true });
